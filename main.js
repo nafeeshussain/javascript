1a885f2de71dbb10155db6cc1fe9b58afc54c356
Nafees Hussain <nafeeshussain719@gmail.com>
2023-09-16 23:24:34 +0500

    First Project

---
+++ main.js
@@ -0,0 +1,38 @@
+/* let x = 5;
+let y = 2;
+let z = x * y;
+
+//Arithmetic Operators
+let a = 3;
+let b = --a;
+
+let xx = 10;
+xx **= 5;
+ */
+
+/* let  x = 10;
+let y = 10;
+let personName = "Bilal";
+
+personName = (personName == "Imtiaz") ? "Bad Person" : "Good Person";
+
+console.log(personName);
+
+
+if(x >= y)
+{
+    console.log("x equal y");
+}
+else{
+    console.log("x not equal y");
+}
+ */
+
+let text1 = "Imtiaz";
+let text2 = "Bilal";
+
+let text3 = "Welcome ";
+text3 += text1;
+text3 += text2;
+
+console.log(text3);
