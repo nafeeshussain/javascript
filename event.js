1f9e0a101d1ef547686b600f420dc1b4572222f4
Nafees Hussain <nafeeshussain719@gmail.com>
2023-09-29 23:02:40 +0500

    Test Commit

--- event.js
+++ event.js
@@ -1,21 +1,33 @@
-function changeColor()
-{
+function changeColor() {
     let h1 = document.getElementById("heading");
     h1.style.color = "red";
 }
-function defaultColor()
-{
+function defaultColor() {
     let h1 = document.getElementById("heading");
     h1.style.color = "black";
 }
 //changeColor();
 
 
-function displayDate()
-{
+function displayDate() {
     document.getElementById("display").innerHTML = Date();
 }
 
-function typeText(text){
+function typeText(text) {
     document.getElementById("heading").innerHTML = text;
 }
+
+
+const cars = ['bmw', 'volvo', 'honda', 'kia'];
+
+cars.map(c => {
+    if (c == 'honda') {
+        let h = `good car ${c} <br>`;
+        document.getElementById('new').innerHTML = h;
+    }
+    else {
+        let n = `bad cars ${c} <br>`;
+        document.getElementById('old').innerHTML += n;
+    }
+});
+
